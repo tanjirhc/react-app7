116624e245ee8892dcc464cb2043050dbfd44c38
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-18 19:44:19 +0600

    a component can hold only one element

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,17 @@
+import { Fragment } from "react";
+
 function ListGroup() {
   return (
-  <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-  </ul>
+    <div>
+      <h1>List</h1> // React.createElement('h1')
+      <ul className="list-group">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+          <li className="list-group-item">A fourth item</li>
+          <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </div>
   );
 }
 
